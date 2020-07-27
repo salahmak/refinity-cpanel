43ab6a80007595493f6746a62e0f297160f2386b
salahmak <crsalah3@gmail.com>
2020-07-27 14:17:20 +0100

    trying to fix deployment problem

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require("express");
 const next = require("next");
 
 const port = process.env.PORT || 3000;
-
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare()
-    .then(() => {
-        const server = express();
-        server.use(express.json());
-        server.use(express.urlencoded({ extended: false }));
-        //server.use('/api', router)
+app.prepare().then(() => {
+    const server = express();
 
-        server.get("*", (req, res) => {
-            return handle(req, res);
-        });
+    // server.get("/a", (req, res) => {
+    //     return app.render(req, res, "/a", req.query);
+    // });
 
-        server.listen(port, (err) => {
-            if (err) throw err;
-            console.log(`>> Ready on http://localhost:${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log(`There was an error starting the server.`);
+    // server.get("/b", (req, res) => {
+    //     return app.render(req, res, "/b", req.query);
+    // });
+
+    server.all("*", (req, res) => {
+        return handle(req, res);
     });
+
+    server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
